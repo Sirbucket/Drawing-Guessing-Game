38f24f9cff9b0e0faeb82b526861f91ec8c8ed3c
Aralthus <51178809+Sirbucket@users.noreply.github.com>
2022-12-08 15:17:51 +0000

    thank you

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 export default {
   server: {
     host: '0.0.0.0',
+    proxy: {
+      './netlify/' : {
+        target : 'https://localhost:9000',
+        pathRewrite : {
+          "^/.netlify/functions" : ""
+        }
+      }
+    }   
   }
 }
